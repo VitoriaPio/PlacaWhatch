beb83d0ea03f666076432e3cad298af068928fd5
muradpontes <muradsp20@gmail.com>
2024-08-25 18:58:20 -0300

    api url

--- front/src/utils/axios-base-config.js
+++ front/src/utils/axios-base-config.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const API_BASE_URL = 'http://localhost:8080/api';
+export const API_BASE_URL = 'https://placa-whatch.vercel.app/api';
 
 export const axiosBaseConfig = axios.create({
   baseURL: API_BASE_URL,
