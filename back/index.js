f379d32217c09955e10039d4015ceac94d1bcd82
muradpontes <muradsp20@gmail.com>
2024-08-26 15:45:17 -0300

    add cors

--- back/index.js
+++ back/index.js
@@ -27,6 +27,12 @@ app.get('/api', (req, res) => {
 
 app.use('/api/placa', placaRota);
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.get('/api/consultar/:placa', (req, res) => {
   const placa = req.params.placa ?? ''
 
