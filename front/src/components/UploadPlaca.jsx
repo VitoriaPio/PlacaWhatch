22adb215b5e7c1d2788322864d0d918ac8f7917b
muradpontes <muradsp20@gmail.com>
2024-08-26 15:30:31 -0300

    rollback

--- front/src/components/UploadPlaca.jsx
+++ front/src/components/UploadPlaca.jsx
@@ -27,11 +27,21 @@ function UploadPlaca() {
     }
   
     try {
-      const result = await registerPlate(formData);
-      setMessage('Placa registrada com sucesso!');
+      const response = await fetch('https://placa-whatch.vercel.app/api/placa/cadastro', {
+        method: 'POST',
+        body: formData,
+      });
+  
+      if (!response.ok) {
+        const errorData = await response.json(); 
+        throw new Error(errorData.error || 'Erro desconhecido');
+      }
+  
+      const result = await response.json();
+      setMessage(result.message);
       console.log('Plate registered successfully:', result);
     } catch (error) {
-      setMessage(`Erro ao registrar a placa: ${error.message}`);
+      setMessage(`Erro ao registrar a placa: ${error.message}`); 
       console.error('Error registering plate:', error.message);
     }
   };
