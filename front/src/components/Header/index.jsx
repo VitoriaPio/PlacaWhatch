a53a94152ab9579c8837096e1a540dbb99a83b7b
VitoriaPio <pio.vitoria156@gmail.com>
2024-09-21 14:40:14 -0300

    refactor: Estilização do header

---
+++ front/src/components/Header/index.jsx
@@ -0,0 +1,40 @@
+import { Link, useNavigate } from 'react-router-dom'
+
+import { useContext } from 'react'
+import { AuthContext } from '../../context/auth-context'
+
+import styles from './Header.module.css'
+
+export default function Header() {
+  const navigate = useNavigate()
+  const { setIsAuthenticated } = useContext(AuthContext)
+
+  function logOut() {
+    setIsAuthenticated(false)
+
+    navigate('/usuario')
+  }
+
+  return (
+    <header className={styles.container}>
+      <h1 className={styles.headerTitle}>Placa Watch</h1>
+      <nav className={styles.headerNav}>
+        <ul className={styles.navList}>
+          <li>
+            <Link to="/upload">Upload de Placa</Link>
+          </li>
+          <li>
+            <Link to="/consulta">Consulta de Placa</Link>
+          </li>
+          <li>
+            <Link to="/relatorio">Relatório por Cidade</Link>
+          </li>
+          <li>
+            <Link to="/tutorial">Tutorial</Link>
+          </li>
+        </ul>
+      </nav>
+      <button onClick={() => logOut()}>Logout</button>
+    </header>
+  )
+}
--- front/src/layout/main-layout/index.jsx
+++ front/src/layout/main-layout/index.jsx
@@ -1,41 +1,13 @@
-import { Link, useNavigate } from 'react-router-dom'
 
-import { useContext } from 'react'
-import { AuthContext } from '../../context/auth-context'
+// components
+import Header from '../../components/Header'
+
 import styles from './main.module.css'
 
 export default function MainLayout({ children }) {
-  const navigate = useNavigate()
-  const { setIsAuthenticated } = useContext(AuthContext)
-
-  function logOut() {
-    setIsAuthenticated(false)
-
-    navigate('/usuario')
-  }
-
   return (
     <div className={styles.container}>
-      <header>
-        <h1>Placa Watch</h1>
-        <nav className="menu">
-          <ul>
-            <li>
-              <Link to="/upload">Upload de Placa</Link>
-            </li>
-            <li>
-              <Link to="/consulta">Consulta de Placa</Link>
-            </li>
-            <li>
-              <Link to="/relatorio">Relatório por Cidade</Link>
-            </li>
-            <li>
-              <Link to="/tutorial">Tutorial</Link>
-            </li>
-          </ul>
-        </nav>
-        <button onClick={() => logOut()}>Logout</button>
-      </header>
+      <Header />
       {children}
     </div>
   )
