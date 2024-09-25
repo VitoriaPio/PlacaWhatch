910be4ff40323d25ffc6408b45082fdd3628422a
VitoriaPio <pio.vitoria156@gmail.com>
2024-09-25 13:56:13 -0300

    Revert "fix: Backend - variaveis de ambiente para o JWT"
    This reverts commit a19f791c92734af2cb32cc3a68cb2f27a87ac39e.

--- back/middleware/jwt.js
+++ back/middleware/jwt.js
@@ -5,11 +5,8 @@ const secretKey = process.env.SECRET_KEY;
 
 function verificarJWT(req, res, next) {
   const token = req.header('x-auth-token');
-
-  console.log(req.headers, token, secretKey)
-
   if (!token) {
-    return res.status(401).json({ error: "Token inexistente" });
+    return res.status(401).json({ error: "Falha na autenticação" });
   }
 
   jwt.verify(token, secretKey, (err, decoded) => {
--- back/routes/placa.js
+++ back/routes/placa.js
@@ -49,6 +49,7 @@ router.post('/cadastro', upload.single('image'),
 
       console.log(horaAtual);
       console.log(dataAtual);
+      let dados;
 
       const form = new formData();
       form.append('file', req.file.buffer, {
@@ -77,6 +78,8 @@ router.post('/cadastro', upload.single('image'),
         return res.status(400).json({ error: 'imagem invalida' });
       }
 
+      // Data response
+      // console.log('API RESPONSE',response.data)
 
       const numero = extrairPlaca(
         response.data.ParsedResults[0].ParsedText
@@ -140,6 +143,8 @@ router.get('/relatorio/cidade/:cidade', jwt, async (req, res) => {
     if (status === 404) {
       return res.status(404).json(registros);
     }
+
+    console.log(registros);
 
     const doc = new PDFDocument();
     res.setHeader('Content-Type', 'application/pdf');
